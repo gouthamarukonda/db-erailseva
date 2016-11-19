404acce66ec5b1a3c24e26ec550d541c8132efb5
Sahil <sahilkonkyana@gmail.com>
2016-11-20 04:46:08 +0530

    Fixed Issue with heroku settings

--- erailseva/settings/base.py
+++ erailseva/settings/base.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 TEMPLATES_DIR = '_TEMPLATES_'
 
 # Quick-start development settings - unsuitable for production
@@ -23,9 +23,9 @@ TEMPLATES_DIR = '_TEMPLATES_'
 SECRET_KEY = 'irid^1ztte_4i)7^3$hz^o6f(^12ccovfe^@fdv^bqk^scd@43'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'erailseva.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
