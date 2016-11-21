f457f3dc0cadee055ae716a358b252e7d2c1457d
Sahil <sahilkonkyana@gmail.com>
2016-11-21 18:11:48 +0530

    bug fix in getstations

--- train/views.py
+++ train/views.py
@@ -27,7 +27,7 @@ def get_stations_for_pnr(request):
 		try:
 			reqdata = json.loads(request.body.decode("utf-8"))
 			qry = "with trainno as (select train_no as trainno from pnr where pnr_no = %s),"
-			qry += "with train_stops as (select station_id, stop_num from stop, trainno where train_no = trainno.trainno) "
+			qry += "train_stops as (select station_id, stop_num from stop, trainno where train_no = trainno.trainno) "
 			qry += "select station_id, station_name from train_stops natural join station order by stop_num"
 			resultset = pgExecQuery(qry, [reqdata["pnr"]])
 			resp = {"status": True}
